4077039ffbfc05169f5b5b8b527a75977654648d
FanchenMeng <mengfc011220@gmail.com>
2025-10-12 21:16:50 +0800

    @time

--- src/WannierExcitonModel.jl
+++ src/WannierExcitonModel.jl
@@ -1,7 +1,4 @@
 module WannierExcitonModel
 
-export test
-test() = println("In future, it will contain the complete ability about calculate exciton based on electronic wannier basis. 
-As for now, this package is empty, and you can use `TightBindingModel.jl`` to use the ability mentioned.")
 
 end
